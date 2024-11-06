932672f486e6117e906a96513c81945c5c2fd49d
Liruihuanpaishou1 <90151449+Liruihuanpaishou1@users.noreply.github.com>
2024-11-06 03:59:46 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
 import os
-import setuptools
 from setuptools import setup, find_packages
-from distutils.core import setup
 
-# Utility function to read the README file.
-
+# Utility function to read the README file
 def read(fname):
-	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    with open(os.path.join(os.path.dirname(__file__), fname), encoding="utf-8") as f:
+        return f.read()
 
 setup(
-	name = "pacific_CodeSwitch",
-	version = "0.1",
-	author = "Jonathan Dunn",
-	author_email = "jonathan.dunn@canterbury.ac.nz",
-	description = ("Detect code switching in Pacific languages"),
-	license = "LGPL 3.0",
-	url = "https://github.com/jonathandunn/pacific_CodeSwitch.git",
-	keywords = "language id, code switching, mri",
-	packages = find_packages(exclude=["*.pyc", "__pycache__"]),
-	package_data={'': ['eng_mri.v24.2.NZ.Multi.Ngrams_v1.50k.50x1_layers.*','eng_mri.*']},
-	install_requires=["cytoolz",
-						"numpy",
-						"scikit-learn",
-						"cleantext",
-						"fasttext",
-						],
-	include_package_data=True,
-	long_description=read('README.md'),
-	long_description_content_type='text/markdown',
-	)
+    name="pacific_CodeSwitch",
+    version="0.1",
+    author="Jonathan Dunn",
+    author_email="jonathan.dunn@canterbury.ac.nz",
+    description="Detect code switching in Pacific languages",
+    license="LGPL 3.0",
+    url="https://github.com/jonathandunn/pacific_CodeSwitch",
+    keywords=["language id", "code switching", "mri"],
+    packages=find_packages(exclude=["*.pyc", "__pycache__"]),
+    package_data={'': ['eng_mri.*']},
+    install_requires=[
+        "cytoolz",
+        "numpy",
+        "scikit-learn",
+        "fasttext"
+    ],
+    include_package_data=True,
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
